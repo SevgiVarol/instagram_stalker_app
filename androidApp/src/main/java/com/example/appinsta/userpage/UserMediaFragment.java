1b6338bf4843c864c3d9fdff51965c83b3918ec8
sevgi <sevgivrl.07@hotmail.com>
2019-12-19 14:05:21 +0300

    used same fragment for user media and my media

--- androidApp/src/main/java/com/example/appinsta/userpage/UserMediaFragment.java
+++ androidApp/src/main/java/com/example/appinsta/userpage/UserMediaFragment.java
@@ -1,19 +1,31 @@
 package com.example.appinsta.userpage;
 
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
 
+import com.bumptech.glide.Glide;
 import com.example.appinsta.R;
+import com.example.appinsta.medialog.MediaLogs;
 import com.example.appinsta.models.DataWithOffsetIdModel;
 import com.example.appinsta.service.InstagramService;
 
@@ -36,6 +48,9 @@ public class UserMediaFragment extends Fragment {
     ProgressBar footerLoadingView;
     AsyncTask getUserMedia;
     DataWithOffsetIdModel dataWithOffsetIdModel;
+    ArrayList<Uri> mediaUrlList;
+    ArrayList<String> mediaIdList;
+    Long userid;
 
     public UserMediaFragment() {
         // Required empty public constructor
@@ -50,11 +65,15 @@ public class UserMediaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_user_media, container, false);
+        view.setBackgroundColor(Color.TRANSPARENT);
+        view.setPadding(10,0,10,30);
         mediasGridView = view.findViewById(R.id.userMediasGridView);
         footerLoadingView = view.findViewById(R.id.footerLoadingView);
         tvInfoText = view.findViewById(R.id.nullMediaInfo);
 
         getUserMedia= new getUserMediaTask().execute();
+        mediaUrlList = new ArrayList<>();
+        mediaIdList = new ArrayList<>();
         return view;
     }
 
@@ -62,10 +81,34 @@ public class UserMediaFragment extends Fragment {
         @Override
         protected String doInBackground(String... strings) {
 
-            if (mediaList.isEmpty()) {
                 dataWithOffsetIdModel = service.getUserMedias(user.getPk());
                 mediaList = dataWithOffsetIdModel.items;
-            }
+                if (mediaUrlList.size() == 0) {
+                    try {
+                        for (int counter = 0; counter < mediaList.size(); counter++) {
+                            if (mediaList.get(counter).getVideo_versions() != null) {
+                                try {
+                                    mediaUrlList.add(Uri.parse(mediaList.get(counter).getVideo_versions().get(0).getUrl()));
+                                } catch (Exception e) {
+                                    //if is post (multiple sharing)
+                                    mediaUrlList.add(Uri.parse(mediaList.get(counter).getCarousel_media().get(0).getVideo_versions().get(0).getUrl()));
+                                }
+                            } else {
+                                try {
+                                    mediaUrlList.add(Uri.parse(mediaList.get(counter).getImage_versions2().getCandidates().get(0).getUrl()));
+                                } catch (Exception e) {
+                                    //if is post (multiple sharing)
+                                    mediaUrlList.add(Uri.parse(mediaList.get(counter).getCarousel_media().get(0).getImage_versions2().getCandidates().get(0).getUrl()));
+                                }
+
+                            }
+                            mediaIdList.add(String.valueOf(mediaList.get(counter).pk));
+                        }
+                    } catch (Exception e) {
+                        Log.e("null object reference", e.getMessage());
+                    }
+                }
+                userid = service.getLoggedUser().pk;
 
             return null;
         }
@@ -102,6 +145,32 @@ public class UserMediaFragment extends Fragment {
 
                         }
                     });
+                    mediasGridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                        @Override
+                        public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                            itemLongClickPopup(position);
+                            return true;
+                        }
+                    });
+
+                    if (user.username.equals(service.getLoggedUser().username)){
+                    mediasGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            if (mediaUrlList != null & mediaUrlList.size() != 0) {
+
+                                Intent mediaLogIntent = new Intent(getActivity(), MediaLogs.class);
+                                mediaLogIntent.putExtra("storyUrlList", mediaUrlList);
+                                mediaLogIntent.putExtra("userId", userid);
+                                mediaLogIntent.putExtra("storyIds", mediaIdList);
+                                mediaLogIntent.putExtra("key", "MEDIA");
+                                mediaLogIntent.putExtra("position", position);
+                                startActivity(mediaLogIntent);
+                            } else {
+                                Toast.makeText(getActivity(), R.string.media_not_found, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });}
 
                 } else {
                     tvInfoText.setVisibility(View.VISIBLE);
@@ -117,6 +186,31 @@ public class UserMediaFragment extends Fragment {
         protected String doInBackground(String... strings) {
             dataWithOffsetIdModel = service.getUserMedias(user.getPk(), dataWithOffsetIdModel.nextMaxId);
             List<InstagramFeedItem> nextMedias = dataWithOffsetIdModel.items;
+            if (mediaUrlList.size() != 0) {
+                try {
+                    for (int counter = 0; counter < nextMedias.size(); counter++) {
+                        if (nextMedias.get(counter).getVideo_versions() != null) {
+                            try {
+                                mediaUrlList.add(Uri.parse(nextMedias.get(counter).getVideo_versions().get(0).getUrl()));
+                            } catch (Exception e) {
+                                //if is post (multiple sharing)
+                                mediaUrlList.add(Uri.parse(nextMedias.get(counter).getCarousel_media().get(0).getVideo_versions().get(0).getUrl()));
+                            }
+                        } else {
+                            try {
+                                mediaUrlList.add(Uri.parse(nextMedias.get(counter).getImage_versions2().getCandidates().get(0).getUrl()));
+                            } catch (Exception e) {
+                                //if is post (multiple sharing)
+                                mediaUrlList.add(Uri.parse(nextMedias.get(counter).getCarousel_media().get(0).getImage_versions2().getCandidates().get(0).getUrl()));
+                            }
+
+                        }
+                        mediaIdList.add(String.valueOf(nextMedias.get(counter).pk));
+                    }
+                } catch (Exception e) {
+                    Log.e("null object reference", e.getMessage());
+                }
+            }
             if (nextMedias != null){mediaList.addAll(nextMedias);}
 
             return null;
@@ -142,5 +236,43 @@ public class UserMediaFragment extends Fragment {
     public void onPause() {
         super.onPause();
         getUserMedia.cancel(true);
+    }
+    public void itemLongClickPopup(int position) {
+        ConstraintLayout layout = new ConstraintLayout(getActivity());
+        ConstraintLayout.LayoutParams clp = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        VideoView videoView = new VideoView(getActivity());
+        ImageView imageView = new ImageView(getActivity());
+        Dialog dialog = new Dialog(getActivity());
+        if (mediaList.get(position).getVideo_versions() != null) {
+
+            Uri videoUri = mediaUrlList.get(position);
+            videoView.setVideoURI(videoUri);
+            videoView.setVisibility(View.VISIBLE);
+            imageView.setVisibility(View.INVISIBLE);
+            videoView.requestFocus();
+            layout.addView(videoView, clp);
+            videoView.start();
+        } else {
+            Uri imageUri = mediaUrlList.get(position);
+
+            Glide.with(getActivity()).load(imageUri).into(imageView);
+            videoView.setVisibility(View.INVISIBLE);
+            imageView.setVisibility(View.VISIBLE);
+            layout.addView(imageView, clp);
+        }
+
+
+        dialog.setContentView(layout, clp);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        dialog.show();
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+            }
+        });
     }
 }
